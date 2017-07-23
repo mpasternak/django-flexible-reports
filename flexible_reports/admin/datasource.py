ddcac4fe93051160282029045a73fd281b169ae3
Michał Pasternak <mpasternak@MacBook-Pro-Michala.local>
2017-07-24 00:27:48 +0200

    validation

--- flexible_reports/admin/datasource.py
+++ flexible_reports/admin/datasource.py
@@ -2,12 +2,40 @@
 
 from django import forms
 from django.contrib import admin
+from django.core.exceptions import ValidationError
+from django.utils.translation import ugettext_lazy as _
+from django_dsl import compiler, exceptions
 
 from .helpers import SmallerTextarea, AverageTextarea
+from .. import utils
 from ..models.datasource import Datasource
 
 
 class DatasourceForm(forms.ModelForm):
+    def clean(self):
+        model = self.cleaned_data['base_model'].model_class()
+        shortcuts = utils.get_shortcuts(model)
+
+        try:
+            filter = compiler.compile(
+                self.cleaned_data['dsl_query'],
+                shortcuts)
+        except exceptions.CompileException:
+            return  # handled by dsl_query.validators
+
+        try:
+            model.objects.filter(filter).first()
+        except Exception as e:
+            raise ValidationError(
+                {"dsl_query": [
+                    ValidationError(
+                        _("An error occured while trying to run the actual "
+                          "database query (%(error)s)"),
+                        params={"error": e}
+                    )
+                ]}
+            )
+
     class Meta:
         widgets = {
             'label': SmallerTextarea,
