5340a2277c08a2d2c47fa4c9a06f795f55eeeb48
Michał Pasternak <mpasternak@MacBook-Pro-Michala.local>
2017-07-24 00:28:47 +0200

    Validator

---
+++ flexible_reports/models/validators.py
@@ -0,0 +1,18 @@
+# -*- encoding: utf-8 -*-
+from django.core.exceptions import ValidationError
+from django.core.validators import RegexValidator
+from django.template import Template
+from django.utils.translation import ugettext_lazy as _
+
+
+def TemplateValidator(value):
+    """Try to compile a string into a Django template"""
+
+    try:
+        Template(value)
+    except Exception as e:
+        raise ValidationError(
+            _("Cannot compile template (%(exception)s)"),
+            params={"exception": e}
+        )
+
