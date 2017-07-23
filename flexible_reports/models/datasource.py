60eb6e2231b40af42cc0ba184c4499a3b7cd83cc
Michał Pasternak <mpasternak@MacBook-Pro-Michala.local>
2017-07-24 00:27:16 +0200

    Use base_model,

--- flexible_reports/models/datasource.py
+++ flexible_reports/models/datasource.py
@@ -1,13 +1,16 @@
 # -*- encoding: utf-8 -*-
 from django.contrib.contenttypes.models import ContentType
+from django.core.exceptions import ValidationError
 from django.db import models
 from django.utils.translation import ugettext_lazy as _
+from django_dsl import compiler
+from django_dsl.exceptions import CompileException
 from django_dsl.fields import DjangoDSLField
 
-from .behaviors import Labelled
+from .behaviors import Labelled, WithBaseModel
 
 
-class Datasource(Labelled):
+class Datasource(Labelled, WithBaseModel):
     """Datasource gets data from the database.
 
     It uses self.base_model.objects.all(), unless there's something in
@@ -17,14 +20,20 @@ class Datasource(Labelled):
     self.base_model.objects.filter().
     """
 
-    base_model = models.ForeignKey(
-        ContentType,
-        verbose_name=_("Base model"),
-    )
-
     dsl_query = DjangoDSLField(
         verbose_name=_("DSL query"))
 
     class Meta:
         verbose_name = _("Datasource")
         verbose_name_plural = _("Datasources")
+
+    def get_model(self):
+        return self.base_model.model_class()
+
+    def get_shortcuts(self):
+        return getattr(self.get_model(), 'django_dsl_shortcuts', {})
+
+    def get_filter(self):
+        return compiler.compile(
+            self.dsl_query,
+            shortcuts=self.get_shortcuts())
