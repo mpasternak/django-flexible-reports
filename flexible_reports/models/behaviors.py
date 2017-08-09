7ae10f496896d831fffc80c2af3f15018cccecef
Michał Pasternak <michal.dtz@gmail.com>
2017-08-10 01:24:47 +0200

    Formatting

--- flexible_reports/models/behaviors.py
+++ flexible_reports/models/behaviors.py
@@ -3,6 +3,7 @@ from django.contrib.contenttypes.models import ContentType
 from django.db import models
 from django.utils.translation import ugettext_lazy as _
 
+
 class WithBaseModel(models.Model):
     base_model = models.ForeignKey(
         ContentType,
@@ -14,19 +15,25 @@ class WithBaseModel(models.Model):
 
 
 class Labelled(models.Model):
-    label = models.TextField()
+    label = models.TextField(
+        verbose_name=_("Label")
+    )
 
     def __str__(self):
         return self.label
 
     class Meta:
         abstract = True
+        ordering = ('label',)
 
 
 class Titled(models.Model):
-    title = models.TextField()
+    title = models.TextField(
+        verbose_name=_("Title")
+    )
 
     subtitle = models.TextField(
+        verbose_name=_("Subtitle"),
         null=True,
         blank=True)
 
@@ -40,7 +47,9 @@ class Titled(models.Model):
 class Orderable(models.Model):
     """Can be placed in an order. Has a position. This field name is the
     default for Grappelli. """
-    position = models.PositiveIntegerField(default=0)
+    position = models.PositiveIntegerField(
+        default=0,
+        verbose_name=_("Position"))
 
     class Meta:
         abstract = True
