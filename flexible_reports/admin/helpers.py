d0a786893de8e9a998daf47c750230269ef40ea0
Michał Pasternak <michal.dtz@gmail.com>
2017-08-10 01:24:02 +0200

    BiggerTextArea for dsl query

--- flexible_reports/admin/helpers.py
+++ flexible_reports/admin/helpers.py
@@ -3,6 +3,7 @@ from django.forms.widgets import Textarea
 
 SmallerTextarea = Textarea(attrs={'cols': 75, 'rows': 2})
 AverageTextarea = Textarea(attrs={'cols': 75, 'rows': 4})
+BiggerTextarea = Textarea(attrs={'cols': 75, 'rows': 18})
 
 try:
     from grappelli.forms import GrappelliSortableHiddenMixin as \
