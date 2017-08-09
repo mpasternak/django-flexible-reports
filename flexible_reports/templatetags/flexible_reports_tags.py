27e376d3cdd000a6c2532c9bb68c7df8238a2274
Michał Pasternak <michal.dtz@gmail.com>
2017-08-10 01:26:00 +0200

    Cool template tag which allows you to type {% flexible report %}

--- flexible_reports/templatetags/flexible_reports_tags.py
+++ flexible_reports/templatetags/flexible_reports_tags.py
@@ -2,12 +2,14 @@
 from django import template
 from django.template.base import Template
 
+from ..adapters.django_tables2 import report as django_tables2_report
+
 register = template.Library()
 
 
 @register.simple_tag(takes_context=True)
 def flexible(context, report):
-    return report.as_html(parent_context=context)
+    return django_tables2_report(report, parent_context=context)
 
 
 @register.simple_tag(takes_context=True)
