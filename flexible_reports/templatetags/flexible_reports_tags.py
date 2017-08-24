ea932242bbd77f8720683bbbff8c225335ab546a
Michał Pasternak <michal.dtz@gmail.com>
2017-08-24 02:10:35 +0200

    Use new function naems

--- flexible_reports/templatetags/flexible_reports_tags.py
+++ flexible_reports/templatetags/flexible_reports_tags.py
@@ -2,14 +2,14 @@
 from django import template
 from django.template.base import Template
 
-from ..adapters.django_tables2 import report as django_tables2_report
+from ..adapters import django_tables2
 
 register = template.Library()
 
 
 @register.simple_tag(takes_context=True)
 def flexible(context, report):
-    return django_tables2_report(report, parent_context=context)
+    return django_tables2.as_html(report, parent_context=context)
 
 
 @register.simple_tag(takes_context=True)
