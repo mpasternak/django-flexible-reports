cec532dfe060bdb45e0330c8ffb6d659430e3fbb
Michał Pasternak <michal.dtz@gmail.com>
2017-08-24 02:09:26 +0200

    Optionally strip HTML on export, Export to XLSX and DOCX

--- flexible_reports/adapters/django_tables2.py
+++ flexible_reports/adapters/django_tables2.py
@@ -4,10 +4,14 @@ import itertools
 import sys
 from collections import OrderedDict
 
+import lxml.html
+from django.http.response import HttpResponse
 from django.template.base import Template, Context
 from django.utils.safestring import mark_safe
 from django_tables2.columns.templatecolumn import TemplateColumn, Column
+from django_tables2.export.export import TableExport
 from django_tables2.tables import Table
+from tablib.core import Databook
 
 
 class CounterMixin:
@@ -41,16 +45,32 @@ class FooterMixin:
             kwargs['footer'] = render_footer
 
 
-class DjangoTables2TemplateColumn(CounterMixin, FooterMixin, TemplateColumn):
-    def __init__(self, display_totals, footer_template, *args, **kw):
+class StripHTMLOnExportMixin:
+    def __init__(self, strip_html_on_export):
+        self.strip_html_on_export = strip_html_on_export
+
+    def value(self, **kwargs):
+        value = super(StripHTMLOnExportMixin, self).value(**kwargs)
+        if self.strip_html_on_export is True:
+            value = lxml.html.fromstring(str(value)).text_content()
+        return value
+
+
+class DjangoTables2TemplateColumn(StripHTMLOnExportMixin, CounterMixin,
+                                  FooterMixin, TemplateColumn):
+    def __init__(self, display_totals, footer_template, strip_html_on_export,
+                 *args, **kw):
         FooterMixin.__init__(self, display_totals, footer_template, kw)
-        TemplateColumn.__init__(self, *args, **kw)
+        StripHTMLOnExportMixin.__init__(self, strip_html_on_export)
         CounterMixin.__init__(self)
+        TemplateColumn.__init__(self, *args, **kw)
 
 
-class DjangoTables2Column(FooterMixin, Column):
-    def __init__(self, display_totals, footer_template, *args, **kw):
+class DjangoTables2Column(StripHTMLOnExportMixin, FooterMixin, Column):
+    def __init__(self, display_totals, footer_template, strip_html_on_export,
+                 *args, **kw):
         FooterMixin.__init__(self, display_totals, footer_template, kw)
+        StripHTMLOnExportMixin.__init__(self, strip_html_on_export)
         Column.__init__(self, *args, **kw)
 
 
@@ -61,7 +81,10 @@ def column(column):
         order_by=column.attr_name,
         display_totals=column.display_totals,
         footer_template=column.footer_template,
-        accessor=column.attr_name
+        accessor=column.attr_name,
+        exclude_from_export=column.exclude_from_export,
+        strip_html_on_export=column.strip_html_on_export,
+        attrs=column.attrs
     )
 
     if column.template:
@@ -89,10 +112,11 @@ def table(table, request, object_list):
         prefix=table.get_prefix(),
         extra_columns=extra_columns,
         request=request,
+        attrs=table.attrs,
         empty_text=mark_safe(table.empty_template))
 
 
-def report(report, parent_context):
+def _report(report, parent_context):
     render_context = copy.copy(parent_context)
     render_context.update({
         'self': report,
@@ -110,7 +134,6 @@ def report(report, parent_context):
 
         table_dict = {
             'title': elem.title,
-            'subtitle': elem.subtitle,
             'object_list': object_list,
             'table': table(
                 elem.table,
@@ -120,4 +143,61 @@ def report(report, parent_context):
 
         render_context['elements'][elem.slug] = table_dict
 
+    return render_context
+
+
+def as_html(report, parent_context):
+    render_context = _report(report, parent_context)
     return Template(report.template).render(render_context)
+
+
+def as_xlsx_databook(report, parent_context):
+    render_context = _report(report, parent_context)
+
+    databook = Databook()
+    for element in render_context['elements'].values():
+        dataset = TableExport(
+            TableExport.XLSX,
+            element['table']
+        ).dataset
+
+        dataset.title = element['title']
+        databook.add_sheet(dataset)
+
+    return databook
+
+
+def as_docx_response(report, parent_context, filename=None):
+    data = as_html(report, parent_context)
+
+    response = HttpResponse(
+        """<head>
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8" /> 
+        </head><body>""" + data + "</body>",
+        content_type='application/vnd.openxmlformats-officedocument.wordprocessingml.document'
+    )
+
+    if filename is None:
+        filename = report.title + ".docx"
+
+    response['Content-Disposition'] = 'attachment; filename="{}"'.format(
+        filename
+    )
+    response['Content-Length'] = len(data)
+    return response
+
+
+def as_xlsx_response(report, parent_context, filename=None):
+    response = HttpResponse(content_type=TableExport.FORMATS[TableExport.XLSX])
+    if filename is None:
+        filename = report.title + ".xlsx"
+
+    response['Content-Disposition'] = 'attachment; filename="{}"'.format(
+        filename
+    )
+
+    xlsx = as_xlsx_databook(report, parent_context)
+    data = xlsx.export(TableExport.XLSX)
+    response['Content-Length'] = len(data)
+    response.write(data)
+    return response
